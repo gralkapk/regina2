65289507b854dd4aac4b83486f8f80b7ba1d2e21
Patrick <->
2017-02-18 13:10:22 +0100

    Initialization of drmgr.

--- src/regina.cpp
+++ src/regina.cpp
@@ -1,19 +1,44 @@
 #include "dr_api.h"
 
+#include "drmgr.h"
+
+#include "log.h"
+
 
 // Forward declarations
 static void event_exit(void);
 //---------------------
 
+/*
+ * dr_client_main
+ */
 DR_EXPORT void dr_client_main(client_id_t id, int argc, const char *argv[]) {
+    dr_log(NULL, LOG_ALL, 1, "Client 'regina' initializing\n");
+    REGINA_LOG("Client 'regina' initializing\n");
+
     // Client setup
-    dr_set_client_name("regina - mem- and call-trace", "-");
+    dr_set_client_name("regina -- mem- and call-trace", "-");
     dr_set_client_version_string("0.1.0");
 
+    // Init extension
+    if (!drmgr_init()) {
+        REGINA_LOG_ERROR("Failed to initialize drmgr\n");
+        return;
+    }
+
+    // Register events
     dr_register_exit_event(event_exit);
+
+    // Notify dr log of this client
+    dr_log(NULL, LOG_ALL, 1, "Client 'regina' is running\n");
+    REGINA_LOG("Client 'regina' is running\n");
 }
 
 
+/*
+ * event_exit
+ */
 static void event_exit(void) {
-
+    // Exit extensions
+    drmgr_exit();
 }
